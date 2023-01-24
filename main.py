cff03d632be85b80ff509b84f3096f71312e446c
fast-campus-ryan <123443225+fast-campus-ryan@users.noreply.github.com>
2023-01-24 22:16:43 +0900

    remove all comments

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hello(name):
+def print_hello(name_):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hello, {name_}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hello, {name_}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hello, {name_}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
