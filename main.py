7791db0dc841966280fc2ab19ad85cdb83e3c535
fast-campus-ryan <123443225+fast-campus-ryan@users.noreply.github.com>
2023-01-24 22:21:25 +0900

    add time

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+import datetime
 
 def print_hello(name_):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name_}')  # Press ⌘F8 to toggle the breakpoint.
-    print(f'Hello, {name_}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hello, {datetime.datetime.utcnow()}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
